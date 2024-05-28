71ed711460c54b7730987419dcd5671a2d8e7c19
dafafauzan01 <107659446+dafafauzan01@users.noreply.github.com>
2024-05-28 22:47:25 +0700

    Delete src/all directory

--- src/all/ehentai/src/eu/kanade/tachiyomi/revived/all/ehentai/Tag.kt
+++
@@ -1,6 +0,0 @@
-package eu.kanade.tachiyomi.revived.all.ehentai
-
-/**
- * Simple tag model
- */
-data class Tag(val name: String, val light: Boolean)
--- src/all/junmeitu/src/eu/kanade/tachiyomi/revived/all/junmeitu/PageDto.kt
+++
@@ -1,6 +0,0 @@
-package eu.kanade.tachiyomi.revived.all.junmeitu
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-class PageDto(val pic: String)
